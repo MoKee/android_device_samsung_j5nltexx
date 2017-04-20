5ad44fdb1772589f9e165f8ded39a6a3ab7260f1
EsromG5 <esromgalaxy5@gmail.com>
2017-04-20 15:42:30 +0000

    mokee-ify

--- mokee.mk
+++ mokee.mk
@@ -1,11 +1,11 @@
-# Inherit some common aokp stuff.
-$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+# Inherit some common Mokee stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device j5nltexx
 $(call inherit-product, device/samsung/j5nltexx/full_j5nltexx.mk)
 
 PRODUCT_DEVICE := j5nltexx
-PRODUCT_NAME := aokp_j5nltexx
+PRODUCT_NAME := mk_j5nltexx
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_RELEASE_NAME := j5nltexx
 TARGET_DEVICE := j5nlte
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo aokp_j5nltexx-userdebug
+add_lunch_combo mk_j5nltexx-userdebug
